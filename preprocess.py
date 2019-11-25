dc3236d2ec57e20c30e542f983cc5747d280026a
Eric <eric_dellavalle@brown.edu>
2019-11-25 17:32:18 -0500

    ok

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,8 @@
 from nba_api.stats.endpoints import playercareerstats
 from nba_api.stats.endpoints import boxscoreadvancedv2
+from nba_api.stats.endpoints import teamgamelog
+import json
+
 import pandas
 # Anthony Davis
 career = playercareerstats.PlayerCareerStats(player_id='203076')
@@ -8,6 +11,9 @@ career = playercareerstats.PlayerCareerStats(player_id='203076')
 
 game_id = '0021700807'
 game_advanced_stats = boxscoreadvancedv2.BoxScoreAdvancedV2(end_period=0, game_id=game_id, start_period=0)
-print(game_advanced_stats.get_json())
+# print(game_advanced_stats.get_json())
 
+team_game_log = teamgamelog.TeamGameLog(season='2018-19', season_type_all_star='Regular Season', team_id='1610612739')
+team_game_log = json.loads(team_game_log.get_json())
+print(team_game_log)
 
