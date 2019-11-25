59db3dfef0864e72ff4528eedad1f97c4c739642
Eric <eric_dellavalle@brown.edu>
2019-11-25 16:41:24 -0500

    ok

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,7 @@
 from nba_api.stats.endpoints import playercareerstats
+from nba_api.stats.endpoints import boxscoreadvancedv2
 import pandas
 # Anthony Davis
 career = playercareerstats.PlayerCareerStats(player_id='203076')
 print(career.get_json())
+
